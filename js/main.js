fce68df8ea76a9220887bfccbde8a76d5693de6b
Basil Jose <basiljose1@gmail.com>
2022-03-31 12:55:40 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "January 29, 2018 00:00:00",
+	date: "January 1, 2023 00:00:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
