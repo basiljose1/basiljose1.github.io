3f0fa3f2224440f6c58d43019b883bf31af3bda5
Basil Jose <basil@yougotagift.com>
2023-09-09 14:15:39 +0530

    Change favicon and add apple touch icon for better user experience

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function myMenuFunction(){
 
 /* ----- TYPING EFFECT ----- */
  let typingEffect = new Typed(".typedText",{
-    strings : ["Developer"],
+    strings : ["Software Engineer", "Python Developer", "Backend Developer"],
     loop : true,
     typeSpeed : 100,
     backSpeed : 80,
